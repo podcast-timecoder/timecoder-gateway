2d952304129a205a2133af2dc5219dcd86bced56
Serhii Pirohov <sergio_89@ukr.net>
2019-02-12 09:36:50 +0200

    patrons service proxy url fix

--- src/main/java/com/example/timecoder/gateway/controller/PatronsController.java
+++ src/main/java/com/example/timecoder/gateway/controller/PatronsController.java
@@ -2,12 +2,10 @@ package com.example.timecoder.gateway.controller;
 
 import com.example.timecoder.gateway.proxy.PatronsServiceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@PreAuthorize("isAuthenticated()")
 public class PatronsController {
 
     @Autowired
--- src/main/java/com/example/timecoder/gateway/proxy/PatronsServiceProxy.java
+++ src/main/java/com/example/timecoder/gateway/proxy/PatronsServiceProxy.java
@@ -3,7 +3,7 @@ package com.example.timecoder.gateway.proxy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "patronsService")
+@FeignClient(name = "patronService", url = "${patronService.ribbon.listOfServers}")
 public interface PatronsServiceProxy {
 
     @RequestMapping("/patrons")
