0af7c4cac15a08858465f8aabc3b646be7563eb5
sergey <sergio_89@ukr.net>
2020-04-01 13:09:37 +0300

    fix sql password

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,7 +18,7 @@ values (
     1,
     'admin@ukr.net',
     'admin',
-    '$2a$10$2zCaORIrNiVpFYRzs7/PTO3FVjx8nWrVbzjesNnHqxLhOSkPpfsAW',
+    '$2a$10$LxEsIfZSh.tX7N.Bthv0wuINJjMpnpVyuDXxuljHDhPejz96Ms.k.',
     'admin');
 
 insert into user_roles (role_id, user_id) values (1, 1);
